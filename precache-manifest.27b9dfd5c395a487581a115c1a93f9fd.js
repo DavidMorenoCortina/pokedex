03e0c5796ca2ac670cc0726ea94ac25e0b104d63
David Moreno Cortina <davidmorenocortina@gmail.com>
2019-04-30 13:56:28 +0200

    Updates

---
+++ precache-manifest.27b9dfd5c395a487581a115c1a93f9fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b78be87a261224df454b5ebdde6265",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "7857474c6a574a646950",
+    "url": "/pokedex/static/css/main.2baf603c.chunk.css"
+  },
+  {
+    "revision": "ed15a524ce05cc5bdd8a",
+    "url": "/pokedex/static/js/2.175b313c.chunk.js"
+  },
+  {
+    "revision": "7857474c6a574a646950",
+    "url": "/pokedex/static/js/main.c1119aaf.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
